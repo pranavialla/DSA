57d4cd09936779dabb59a1964169031d2321ecc1
apranavi <apranavi@tekion.com>
2024-04-03 13:49:57 +0530

    Initial commit

---
+++ src/Arrays/ZeroMatrix.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.io.File;
+
+public class ZeroMatrix {
+
+    ZeroMatrix(){
+        execute();
+    }
+    void execute(){
+        // setting  row 0
+        for(int column=0; column<=n; column++){
+            arr[m][j]=0;
+        }
+
+        // setting  column 0
+        for(int row=0; row<=m; row++){
+            arr[i][n]=0;
+        }
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File inputFile = new File("IOFiles/Input.txt");
+        File outputFile = new File("IOFiles/Output.txt");
+
+
+
+    }
+}
