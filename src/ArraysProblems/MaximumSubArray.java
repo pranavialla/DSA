75e6c56eaffa565bf5a2bc85410ea957aa77dce0
pranavialla <47995889+pranavialla@users.noreply.github.com>
2025-08-23 01:01:38 +0530

    Update MaximumSubArray.java

--- src/ArraysProblems/MaximumSubArray.java
+++ src/ArraysProblems/MaximumSubArray.java
@@ -5,9 +5,8 @@ public class MaximumSubArray {
     public int maxSubArray(int[] nums) {
         int curMaximumSubArray=nums[0];
         int maximumSubArraySoFar=nums[0];
-        int prevCurMax=nums[0];
         for(int i=1; i<nums.length ; i++){
-            curMaximumSubArray=Math.max(nums[i], prevCurMax + nums[i]) ;
+            curMaximumSubArray=Math.max(nums[i], curMaximumSubArray + nums[i]) ;
             maximumSubArraySoFar=Math.max(maximumSubArraySoFar, curMaximumSubArray);
         }
         return maximumSubArraySoFar;
