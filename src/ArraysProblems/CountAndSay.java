63be93a619ab508c721110c0f7613759c0de7584
pranavialla <47995889+pranavialla@users.noreply.github.com>
2025-08-25 15:10:26 +0530

    Create CountAndSay.java

---
+++ src/ArraysProblems/CountAndSay.java
@@ -0,0 +1,44 @@
+// Iterative function to return the nth term
+class CountAndSay {
+    
+    public static String countAndSay(int n) {
+        
+        if (n == 1) return "1";
+
+        String curr = "1";
+
+        // Start from the second term, build every term
+        // terms using the previous term
+        for (int i = 2; i <= n; i++) {
+            StringBuilder next = new StringBuilder();
+            int cnt = 1;
+
+            for (int j = 1; j < curr.length(); j++) {
+                
+                // If same as previous, then increment
+                // count
+                if (curr.charAt(j) == curr.charAt(j - 1)) {
+                    cnt++;
+                    
+                // If different process the previous 
+                // character and its count and reset
+                // count for the current character
+                } else {
+                    next.append(cnt).append(curr.charAt(j - 1));
+                    cnt = 1;
+                }
+            }
+
+            next.append(cnt).append(curr.charAt(curr.length() - 1));
+            curr = next.toString();
+        }
+
+        return curr;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countAndSay(n));
+    }
+}
